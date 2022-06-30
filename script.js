b3b402decf2c274927895733f05b081a03b044ae
sabirvlog <sabirkhan0342464@gmail.com>
2022-06-30 15:39:00 +0500

    main

---
+++ script.js
@@ -0,0 +1,26 @@
+// This is JavaScript code .....
+console.log("My name is sabir");
+console.log(34 + 50);
+console.log("25 + 4");
+console.log([34, 45, 85, 8, 2]);
+console.warn("This a very risky task...  warning!");
+console.table({ sabir: 45, sabir: 45, sabir: 45, sabir: 45 });
+// ***** console commands **********....
+
+console.time();
+
+var submit = "This is a sabir khan";
+document.getElementById("zero").innerHTML = submit;
+
+var String = "<br><br>" + "My name is Sabir Khan";
+document.getElementById("hero").innerHTML = String;
+
+var arr = ["sabir", "aslam", "ali", "Omar", "imran"];
+document.getElementById("lezo").innerHTML = arr;
+
+var a = 34;
+var b = 75;
+var adding = a + b;
+document.getElementById("mero").innerHTML = adding;
+
+console.timeEnd();
